37dbc9aebc58b3c9c5c416a5150742858f3ebd9e
Shubham <shubhamv2010@gmail.com>
2022-01-04 15:19:00 +0530

    Update

---
+++ Project/list_grid/src/components/list.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>List Display</h1>
+</template>
--- Project/modal-project/src/App.vue
+++ Project/modal-project/src/App.vue
@@ -1,19 +1,39 @@
 <template>
   <!-- all template are injected into index.html -->
   <h1>{{ title }}</h1>
+  <br />
+  <!-- <input type="text" ref="name" />
+  <br />
+  <button @click="handleclick">Click Me</button> -->
+  <Modal :header="header" :text="text" theme="sale1" />
 </template>
 
 <script>
+// 1st import the component required
+import Modal from "./components/Modal.vue";
 export default {
   name: "App",
+  //2nd use modal
+  components: { Modal },
   data() {
     return {
       title: "My First App :)",
+      header: "Sign up For Giveaway",
+      text: "Grab the Items at half price",
     };
+  },
+  methods: {
+    handleclick() {
+      console.log(this.$refs.name);
+      // adding a class name to ref element
+      this.$refs.name.classList.add("active");
+      //adding a focus to the ref element
+      this.$refs.name.focus();
+    },
   },
 };
 </script>
-
+//dont scoped this main style
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
